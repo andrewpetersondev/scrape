b794eb86a7508a77aaf48ea6a8f0d6b4b0c88d8e
Andrew P <andrewpetersondev@gmail.com>
2019-08-28 22:42:51 -0500

    added config and middleware, one route, and listener to start the server

--- server.js
+++ server.js
@@ -7,9 +7,20 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 // configuration and middleware
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(morgan("dev")); // use morgan to log results
 app.use(express.urlencoded({ extended: true })); // parse request body as json
 app.use(express.json()); // parse request body as json
 app.use(express.static("public")); // make a public static folder
+mongoose.connect('mongodb://localhost/mongoHeadlines', { useNewUrlParser: true });
+
+// routes
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
+// start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!\n");
+});
