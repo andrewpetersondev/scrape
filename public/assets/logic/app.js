fbebcb99484a1e08c849340e19cdfa2d2e7341c7
Andrew P <andrewpetersondev@gmail.com>
2019-08-29 09:12:17 -0500

    added jquery link, dynamically generated cards, and empty page message

--- public/assets/logic/app.js
+++ public/assets/logic/app.js
@@ -1,17 +1,34 @@
 // global variables
 // ======================================================================
-
+const articleContainer = $(".article-container");
 
 
 
 // functions
 // ======================================================================
 
-// function loadPage(){}
+function loadPage() {
+
+}
 
 // function renderArticles(articles){}
 
-// function createCard(article){}
+function showEmptyPageMessage() {
+    let emptyMessage = $("<div class='alert text-center'><h4>There are no articles to display. Click the scrape button at the top of the screen to see more articles.</h4></div>");
+    articleContainer.append(emptyMessage);
+}
+
+function createCard(article) {
+    let card = $("<div class='card'>"); // add data-id="whatever" so that each card can be selected
+    let cardHeader = $("<div class='card-header'>");
+    let cardHeaderTitle = $("<h3>");
+    let cardHeaderLink = $("<a class='article-link' target='_blank' rel='noopener noreferrer'>").attr("href", article.link).text(article.title);
+    let saveButton = $("<a class='btn btn-success save'>Save Article</a>");
+    let cardBody = $("<div class='card-body'>").text(article.summary);
+    cardHeader.append(cardHeaderTitle, cardHeaderLink, saveButton);
+    card.append(cardHeader, cardBody);
+    card.data("_id", article._id);
+}
 
 // function saveArticle(){}
 
@@ -33,14 +50,16 @@
 // events
 // ======================================================================
 $(document).ready(function () {
+    console.log("document is ready");
 
     // grab the articles as a json
-    $.getJSON("/articles", function (data) {
+    jQuery.getJSON("/articles", function (data) {
         for (let i = 0; i < data.length; i++) {
-            $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+            $(".article-container").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
         }
     });
-    // click a p tag
+
+    // click an article
 
     // click save note button
 
